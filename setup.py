116f92696757709edf5c720f459957cc2153676b
Cameron-Ross <cameron.edward.ross@gmail.com>
2023-06-20 20:11:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='stellarutil',
-    version='0.0.6',
+    version='0.0.7',
     author='Cameron Ross',
     author_email='ceross@cpp.edu',
     description= 'A utility package for CPP Fire Squad',
+    packages=find_packages(),
     license='MIT',
     url='https://github.com/CPP-FIRE-Squad/stellarutil',
     py_modules=['console', 'graph', 'simulation', 'calculations'],
@@ -17,3 +18,23 @@ setup(
     },
     
 )
+
+
+# setup(
+#     name='mypkg',
+#     version='0.1',
+#     packages=find_packages(),
+#     package_data={'spam': ['data.txt']},
+#     py_modules=['bacon']
+# )
+
+# root
+# ├── spam
+# │   ├── __init__.py
+# │   ├── data.txt
+# │   ├── eggs.py
+# │   └── fizz
+# │       ├── __init__.py
+# │       └── buzz.py
+# ├── bacon.py
+# └── setup.py
