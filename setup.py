6da49c4e9081a82f12b40a251c98a876403902a2
Cameron Ross <cameronross@MacBook-Air.lan>
2023-07-18 19:59:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='stellarutil',
-    version='0.0.40',
+    version='0.0.41',
     author='Cameron Ross',
     author_email='ceross@cpp.edu',
     description= 'A utility package for CPP Fire Squad',
     packages=find_packages(),
     license='MIT',
     url='https://github.com/CPP-FIRE-Squad/stellarutil',
-    # py_modules=['console', 'graph', 'simulation', 'calculations'],
+    py_modules=['console', 'graph', 'simulation', 'calculations'],
     entry_points={
         'console_scripts': [
             'stellarutil = stellarutil.console:command_line_interact',
