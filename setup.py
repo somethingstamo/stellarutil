ad9802e019af82e06967710e697dfe2518330897
Cameron Ross <cameron.edward.ross@gmail.com>
2023-11-29 21:15:30 -0800

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+
+
 setup(
     name='stellarutil',
-    version='0.0.92',
+    version='0.0.93',
     author='Cameron Ross',
     author_email='ceross@cpp.edu',
     description= 'A utility package for CPP Fire Squad',
