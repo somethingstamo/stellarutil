195f497ada73dd2b9506373b669bd96d92b2945c
Austin Martinez <89665806+AyeJayM@users.noreply.github.com>
2024-03-06 22:43:05 -0800

    Incremented version number to '0.1.8'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='stellarutil',
-    version='0.1.7',
+    version='0.1.8',
     author='Cameron Ross',
     author_email='ceross@cpp.edu',
     description= 'A utility package for CPP Fire Squad',
